9cca5384a64f1e4a980f07e6d184ece3cf8f0a12
Rich Harris <richard.a.harris@gmail.com>
2013-05-10 15:05:41 +0100

    updated to 0.1.1

--- js/main.js
+++ js/main.js
@@ -4,28 +4,48 @@ window.addEventListener( 'load', function () {
 
 	'use strict';
 
+	var id, colors, randomColor = function () {
+		var red, green, blue;
+
+		red = Math.floor( Math.random() * 256 );
+		green = Math.floor( Math.random() * 256 );
+		blue = Math.floor( Math.random() * 256 );
+
+		return 'rgb(' + [ red, green, blue ].join( ',' ) + ')';
+	};
+
 	divvy = new Divvy({
 		el: document.getElementById( 'inner' ),
 		columns: [
 			{
-				"id": "left",
-				"size": 50,
-				"min": 100,
-				"max": 300,
-				"children": [
-					"copy",
-					"console",
-					[ 'left', 'right' ]
+				id: "left",
+				size: 50,
+				min: 100,
+				children: [
+					'left-top',
+					'left-middle',
+					[ 'left-bottom-left', 'left-bottom-right' ]
 				]
 			},
 			{
-				"id": "right",
-				"size": 50,
-				"children": [
-					"top", "bottom"
+				'id': "right",
+				'size': 50,
+				'children': [
+					'right-top', 'right-bottom'
 				]
 			}
 		]
 	});
+
+	colors = {
+		'left-top': 'rgb(220,240,220)',
+		'right-bottom': 'rgb(230,210,240)'
+	};
+
+	for ( id in divvy.blocks ) {
+		if ( divvy.blocks.hasOwnProperty( id ) ) {
+			divvy.blocks[ id ].style.backgroundColor = colors[ id ] || randomColor();
+		}
+	}
 
 });
