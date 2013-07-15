a9d903cf1ba378588128cb39e8f648923e4744dc
Rich Harris <richard.a.harris@gmail.com>
2013-07-15 08:41:24 +0100

    requirejs optimiser bugfix

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,8 +20,8 @@ module.exports = function ( grunt ) {
 		concat: {
 			options: {
 				banner: '// Divvy v<%= pkg.version %>\n// Copyright (2013) Rich Harris\n// Released under the MIT License\n\n// https://github.com/Rich-Harris/Divvy\n\n;(function ( global ) {\n\n\'use strict\';\n\n',
-				footer: '\n\nif ( typeof global.module !== "undefined" && global.module.exports ) { global.module.exports = Divvy; }\n' +
-					'else if ( typeof global.define !== "undefined" && global.define.amd ) { global.define( function () { return Divvy; }); }\n' +
+				footer: '\n\nif ( typeof module !== "undefined" && module.exports ) { module.exports = Divvy; }\n' +
+					'else if ( typeof define !== "undefined" && define.amd ) { define( function () { return Divvy; }); }\n' +
 					'else { global.Divvy = Divvy; }\n\n}( this ));'
 			},
 			build: {
